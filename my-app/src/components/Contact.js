e78d053db2c71973bc65949f39e2151dcd1c706c
Emily Koranda <ekoranda@Emilys-MacBook-Pro.local>
2022-02-13 01:11:43 -0600

    changed name

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -55,12 +55,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                placeHolder@domain.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">555-456-7890</p>
             </div>
           </div>
         </div>
