e7ff5d5bc6fb6dd78494497c4e93851867a29ece
Emily Koranda <ekoranda@Emilys-MacBook-Pro.local>
2022-04-20 23:37:46 -0500

    some animations

--- src/components/About.js
+++ src/components/About.js
@@ -3,34 +3,109 @@ import "../styles.css";
 import girl1 from "./img/casual-life-3d-female-metis-t-shirt-pose-notebook-4 (1).png";
 
 import { BsGithub, BsLinkedin, BsArrowRight } from 'react-icons/bs';
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import TypeWriter from 'react-typewriter';
+
 
 
 export default function About() {
+  const line1 = "I'm Emily Koranda"
+  const line2 = "Front-end"
+  const line3 = "Engineer"
+
+  const container = {
+    hidden: { opacity: 0 },
+    show: {
+      
+      opacity: 1,
+      transition: {
+        y: ['100px', '0px'],
+        delay: 1.75,
+      }
+    },
+    active: {
+      y: ['100px', '0px'],
+      duration: 0.5,
+      delay: 2.3,
+    }
+  }
+
+  const variants = {
+    hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      y: ['-10px', '0px'],
+      transition: {
+        delay: 2,
+      }
+    },
+  }
+
+  const paragraph = {
+    hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      y: ['5px', '0px'],
+      transition: {
+        delay: 2.2,
+      }
+    },
+  }
+
+  const links = {
+    hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      y: ['5px', '0px'],
+      transition: {
+        delay: 2.4,
+      }
+    },
+  }
+
+  const girl = {
+    hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      x: ['300px', '0px'],
+      transition: {
+        delay: 1,
+      }
+    },
+  }
+
   return (
     <section id="emily">
       <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 ">
-        <motion.div 
-        animate={{
-          x: ['100px' , '0px']    
-        }}
-        transition={{
-          type: 'tween',
-          duration: 1
-        }}
+        <motion.div
+            initial="hidden"
+            animate="visible"            
+            variants={variants}
         >
-<p className="text-danger mb-2 hover:animate-bounce">UI/UX Design</p>
-</motion.div>
-          
-          <h1 className="sm:text-4xl text-6xl mb-4 font-medium text-bleachWhite">
+        <p className="text-danger mb-2 hover:animate-bounce">UI/UX Design</p>
+        </motion.div>
+        <TypeWriter typing={1} initDelay={0} maxDelay={100} className="typedHeader">
+        <h1 className="sm:text-4xl text-6xl mb-4 font-medium text-bleachWhite">
           I'm <span className="animate-charcter">Emily</span>,
             <br className="hidden lg:inline-block" /><span className="underline-primary">Front-End</span>
             <br className="hidden lg:inline-block" />Software Engineer
           </h1>
+        </TypeWriter> 
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={paragraph}
+        >
           <p className="mb-0 mt-6 text-secondary line-height-35 pr-20">
           I’m a Front-End Software Engineer located in  Madison, Wisconsin pursing a Bachelor's of Science in Computer Science. I have a passion for web design and building creating and intuitive, dynamic user experiences.
           </p>
+          </motion.div>  
+          <motion.div
+            initial="hidden"
+            animate="visible"            
+            variants={links}
+        > 
           <div className="flex justify-center mb-4 ml-12 mt-5">
             <ul>
               <li>
@@ -45,7 +120,12 @@ export default function About() {
               </li>
             </ul>
           </div>
-        
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            animate="visible"            
+            variants={links}
+        > 
           <div className="flex justify-center">
             <a
             href={require("./img/Emily Koranda Resume.pdf")} download="EmilyKorandaResume"
@@ -54,10 +134,17 @@ export default function About() {
               <BsArrowRight className="text-2xl font-black ml-6 mt-1 hover:font-semibold"></BsArrowRight>
             </a>
           </div>
+          </motion.div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 relative">
           <div className="pink-gradient z--1">
+          <motion.div
+            initial="hidden"
+            animate="visible"            
+            variants={girl}
+        > 
           <img src={girl1} alt="logo" className="z-1"/>
+          </motion.div>
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,122 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../styles.css";
 import girl2 from "./img/casual-life-3d-woman-taking-a-funny-photo-with-dog-1 (1).png";
 import { MdSchool } from 'react-icons/md';
 import { GiBullseye } from 'react-icons/gi';
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
+
+const squareVariants = {
+  hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      y: ['5px', '0px'],
+      transition: {
+        delay: 0.2,
+        duration: 1.5,
+      }
+    },
+};
+
+
+const sideVar = {
+  hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      x: ['200px', '0px'],
+      transition: {
+        delay: 1,
+        duration: 1.5,
+      }
+    },
+};
+
+const girlVar = {
+  hidden: {opacity: 0},
+    visible:{
+      opacity: 1,
+      transition: {
+        delay: 0,
+        duration: 2,
+      }
+    },
+};
+
+function Side() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={sideVar}
+    >
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 relative flex flex-col items-center ">
+     <MdSchool className="text-secondary text-10xl"></MdSchool>
+          <p className="text-white px-20 line-height-35 font-medium mb-10 mt-2">
+            I'm a current senior at UW-Madison pursuing a Bachelor's of Science in Computer Science and a certificate in Digital Studies.
+          </p>
+          <GiBullseye className="text-secondary text-10xl"></GiBullseye>
+          <p className="text-white px-20 line-height-35 font-medium mb-10 mt-2"> I want to combine my formal computer science education with my passion for digital design and creativity to create innovative and imaginative user experiences.</p>
+      </div>
+    </motion.div>
+  );
+}
+
+function Title() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={squareVariants}
+    >
+
+      
+      <h6 className="text-primary mb-0">A little bit about me!</h6>
+          <h1 className="sm:text-4xl text-3xl font-semibold mb-0 text-white">
+            Who I Am.
+          </h1>
+    </motion.div>
+  );
+}
+
+
+function Girl() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      variants={girlVar}
+    >
+
+      
+    <img src={girl2} alt="logo" className="z-1 w-3/4 ml-32"/>
+    </motion.div>
+  );
+}
 
 
 export default function Projects() {
@@ -11,28 +124,23 @@ export default function Projects() {
     <section  id="about" className="text-white bg-dark body-font mt-5">
       <div className="container px-5 pt-10 mx-auto text-center lg:px-40 pb-0">
         <div className="flex flex-col w-full mb-10">
-          <h6 className="text-primary mb-0">A little bit about me!</h6>
-          <h1 className="sm:text-4xl text-3xl font-semibold mb-0 text-white">
-            Who I Am.
-          </h1>
+ 
+          <Title />
         </div>
        
       </div>
       <div className="container mx-auto flex px-10  md:flex-row flex-col items-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <div className="pink-gradient justify-items-center">
-          <img src={girl2} alt="logo" className="z-1 w-3/4 ml-32"/>
+            <Girl />
+         
           </div>
       </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 relative flex flex-col items-center ">
-          <MdSchool className="text-secondary text-10xl"></MdSchool>
-          <p className="text-white px-20 line-height-35 font-medium mb-10 mt-2">
-            I'm a current senior at UW-Madison pursuing a Bachelor's of Science in Computer Science and a certificate in Digital Studies.
-          </p>
-          <GiBullseye className="text-secondary text-10xl"></GiBullseye>
-          <p className="text-white px-20 line-height-35 font-medium mb-10 mt-2"> I want to combine my formal computer science education with my passion for digital design and creativity to create innovative and imaginative user experiences.</p>
+        
+          <Side />
+         
 
-        </div>
+       
       </div>
     </section>
   );
